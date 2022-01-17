7ebbe539b9ed7b695db4ebe87a0cb759d59d4ddb
Gomez0015 <oscargomez.raxo@gmail.com>
2022-01-17 22:58:39 +0100

    Fix Multiple Instances with Bot

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -118,6 +118,7 @@ exports.startFarming = async function(res, req) {
 
             // Update the count down every 1 second
             var x = setInterval(function() {
+                console.log(countDownDistance); // Checking if the timer is still running after destroying the bot
                 // Get today's date and time
                 var now = new Date().getTime();
                 // Find the distance between now and the count down date
@@ -128,7 +129,7 @@ exports.startFarming = async function(res, req) {
             let channelIdToCheck = req.body.channelId;
 
             client.on("message", async function(message) {
-                var checkIfBotNeedsShutdown = allFarmData.find(obj => {
+                let checkIfBotNeedsShutdown = await allFarmData.find(obj => {
                     return (obj.discordId === req.body.userToken && obj.botName === client.user.tag)
                 });
 
