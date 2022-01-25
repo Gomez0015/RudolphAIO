521100e42ff67bf54ee9042f769c0559da2bed73
Gomez0015 <oscargomez.raxo@gmail.com>
2022-01-26 00:10:57 +0100

    Dad jokes on welcome

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -7,6 +7,8 @@ module.exports = {
         console.log("User " + member.user.tag + " has joined the server!");
 
         //Find a channel named welcome and send a Welcome message
-        bot.channels.cache.get('927639271597305987').send("Welcome To the North Pole! " + member.toString() + " \n heres a joke for ya " + giveMeAJoke.getRandomDadJoke());
+        giveMeAJoke.getRandomDadJoke().then(joke => {
+            bot.channels.cache.get('927639271597305987').send("Welcome To the North Pole! " + member.toString() + " \nheres a joke for ya " + joke);
+        });
     }
 };
