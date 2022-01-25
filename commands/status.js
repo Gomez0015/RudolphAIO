5f89cdb25066e2a24fd45b1ea8a50d6198f08d09
Gomez0015 <oscargomez.raxo@gmail.com>
2022-01-25 17:42:51 +0100

    Fixed Server Status Command

--- commands/status.js
+++ commands/status.js
@@ -5,7 +5,7 @@ exports.run = async(bot, message, args) => {
     http.get('http://example.com/category', function(res) {
         // If you get here, you have a response.
         // If you want, you can check the status code here to verify that it's `200` or some other `2xx`.
-        var msg = res.statusCode === 200 ? 'host \`' + host + '\` is alive' : 'host \`' + host + '\ is dead';
+        var msg = res.statusCode === 200 ? 'host \`' + host + '\` is alive' : 'host \`' + host + '\` is dead';
         message.channel.send("Status: " + msg);
     }).on('error', function(e) {
         console.log(e);
