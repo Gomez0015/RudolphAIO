686935298c969828a1f866e99b70a595ce89edd5
Gomez0015 <oscargomez.raxo@gmail.com>
2022-01-25 19:08:54 +0100

    fixed status channel

--- events/ready.js
+++ events/ready.js
@@ -12,7 +12,7 @@ module.exports = {
         bot.user.setPresence({ activities: [{ name: 'Delivering Whitelists around the globe!' }] });
 
         var host = 'beta.rudolphaio.com';
-        var statusChannel = bot.channels.get("935478854049484810");
+        var statusChannel = bot.channels.cache.get("935478854049484810");
         let lastState = 1;
 
         cron.schedule('*/30 * * * *', () => {
