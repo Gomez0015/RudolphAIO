dfa69905ef035669908362b92f1fe3627252b213
Gomez0015 <oscargomez.raxo@gmail.com>
2022-01-25 17:39:25 +0100

    Fixed Server Status Command

--- commands/status.js
+++ commands/status.js
@@ -1,10 +1,14 @@
-const ping = require('ping');
+const http = require('http');
 
 exports.run = async(bot, message, args) => {
     var host = 'beta.rudolphaio.com';
-    ping.sys.probe(host, function(isAlive) {
-        var msg = isAlive ? 'host \`' + host + '\` is alive' : 'host ' + host + ' is dead';
+    http.get('http://example.com/category', function(res) {
+        // If you get here, you have a response.
+        // If you want, you can check the status code here to verify that it's `200` or some other `2xx`.
+        var msg = res.statusCode === 200 ? 'host \`' + host + '\` is alive' : 'host \`' + host + '\ is dead';
         message.channel.send("Status: " + msg);
+    }).on('error', function(e) {
+        console.log(e);
     });
 }
 
