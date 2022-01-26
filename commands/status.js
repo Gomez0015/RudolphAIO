132400086f0ae7b49708f961966b5c84ce5c50bc
Gomez0015 <oscargomez.raxo@gmail.com>
2022-01-26 09:17:28 +0100

    Dad jokes on welcome

--- commands/status.js
+++ commands/status.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 exports.run = async(bot, message, args) => {
     var host = 'beta.rudolphaio.com';
-    http.get('http://example.com/category', function(res) {
+    http.get(host, function(res) {
         // If you get here, you have a response.
         // If you want, you can check the status code here to verify that it's `200` or some other `2xx`.
         var msg = res.statusCode === 200 ? 'host \`' + host + '\` is alive' : 'host \`' + host + '\` is dead';
