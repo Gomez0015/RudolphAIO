086245f9c5024652d286847fb3d4274248501481
Gomez0015 <oscargomez.raxo@gmail.com>
2022-01-31 19:26:29 +0100

    MEE6 Rank Card Generator

--- src/components/Generators.js
+++ src/components/Generators.js
@@ -22,7 +22,7 @@ function Generators() {
         botColor: '#fff'
     });
 
-    const randomValue = Math.random();
+    const randomValue = Math.floor(Math.random());
 
     const numberWithCommas = (x) => {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
