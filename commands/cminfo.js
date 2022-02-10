e30542f1332339114ba5288a599327fcc25182ae
Gomez0015 <oscargomez.raxo@gmail.com>
2022-02-10 13:10:30 +0100

    Fix cminfo 0.1

--- commands/cminfo.js
+++ commands/cminfo.js
@@ -1,4 +1,5 @@
 const candyMachineScraper = require('candymachinescraper');
+const { MessageEmbed } = require('discord.js');
 
 exports.run = async(bot, message, args) => {
     let url = args[0];
@@ -6,11 +7,21 @@ exports.run = async(bot, message, args) => {
         let responseData = 'Hello World!';
         if (data.state == 'error') {
             responseData = data.message;
+            message.reply(data.state + ": " + responseData);
         } else {
             responseData = data.data;
+            const candyEmbed = new MessageEmbed()
+                .setColor('#221f20')
+                .setTitle('Scraped Candy Machine Info')
+                .setURL(url)
+                .addFields({ name: 'ID', value: responseData })
+                .addField('Inline field title', 'Some value here', true)
+                .setTimestamp()
+                .setAuthor({ name: 'Raxo#0468', iconURL: 'https://avatars.githubusercontent.com/u/56361339?v=4', url: 'https://github.com/Gomez0015' })
+
+            message.reply({ embeds: [candyEmbed] });
         }
 
-        message.reply(data.state + ": " + responseData);
         // {
         //   state: 'success',
         //   data: '2QcWbuQTyfEDdHHhpgfoXfptkFipE5J1SqQiZxvZERuR'
