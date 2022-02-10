d13d399afae8ea6d432bc91bd86b9e4148c9f6af
Gomez0015 <oscargomez.raxo@gmail.com>
2022-02-10 12:48:25 +0100

    Added cminfo v1.0

---
+++ commands/cminfo.js
@@ -0,0 +1,18 @@
+const candyMachineScraper = require('candymachinescraper');
+
+exports.run = async(bot, message, args) => {
+    let url = args[0];
+
+    await candyMachineScraper.getScript('../node_modules', url, function(data) {
+        message.reply(data.state + ": " + data.message);
+        // {
+        //   state: 'success',
+        //   data: '2QcWbuQTyfEDdHHhpgfoXfptkFipE5J1SqQiZxvZERuR'
+        // }
+    });
+}
+
+exports.info = {
+    name: "cminfo",
+    description: "Get Candy Machine Info"
+}
