873ff181cfa7b1d10b521dc72b3b8f38afadfac6
Gomez0015 <oscargomez.raxo@gmail.com>
2022-03-03 22:24:12 +0100

    Added Collection Monitor

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -9,7 +9,7 @@ exports.checkMonitors = async function(bot) {
             const response = await axios.get(`https://api-mainnet.magiceden.dev/v2/collections/${collection.data}/stats`);
             console.log(response.data);
             if ((response.data.floorPrice / 1000000000) <= 2) {
-                let user = bot.users.fetch(item.discordId);
+                let user = await bot.users.fetch(item.discordId);
                 console.log(user);
                 user.send(`😱 ${collection.data} floor price is down to ${response.data.floorPrice} SOL !!`);
             }
