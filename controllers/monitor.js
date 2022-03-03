de7ef9db7b12a5e76caafbdc7443b6ff5ac977ea
Gomez0015 <oscargomez.raxo@gmail.com>
2022-03-03 22:18:08 +0100

    Added Collection Monitor

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -2,14 +2,14 @@ const dashboardKeys = require('../commands/models/dashboardKeysModel.js');
 const axios = require('axios');
 
 exports.checkMonitors = async function(bot) {
-    const dashboardKeys = await dashboardKeys.find({});
+    const dashboardKeysData = await dashboardKeys.find({});
 
-    await dashboardKeys.forEach(async function(item) {
+    await dashboardKeysData.forEach(async function(item) {
         item.monitors.collections.forEach(async function(collection) {
             const response = await axios.get(`https://api-mainnet.magiceden.dev/v2/collections/${collection.data}/stats`);
             console.log(response);
             if ((response.floorPrice / 1000000000) <= 2) {
-                let user = client.users.cache.fetch(dashboardKeys.discordId);
+                let user = client.users.cache.fetch(item.discordId);
                 user.send(`😱 ${collection.data} floor price is down to ${response.floorPrice} SOL !!`);
             }
         });
