039c1453643dcee7b0ffb6c7f719595dfce4c280
Gomez0015 <oscargomez.raxo@gmail.com>
2022-03-03 22:39:28 +0100

    Added Collection Monitor

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -7,12 +7,20 @@ exports.checkMonitors = async function(bot) {
     await dashboardKeysData.forEach(async function(item) {
         item.monitors.collections.forEach(async function(collection) {
             const response = await axios.get(`https://api-mainnet.magiceden.dev/v2/collections/${collection.data}/stats`);
-            console.log(response.data);
-            if ((response.data.floorPrice / 1000000000) <= 2) {
-                let user = await bot.users.fetch(item.discordId);
-                console.log(user);
-                user.send(`😱 ${collection.data} floor price is down to ${response.data.floorPrice / 1000000000} SOL !!`);
+            if ((response.data.floorPrice / 1000000000) <= collection.floorLow) {
+                if (collection.lastSent != 'floorLow') {
+                    let user = await bot.users.fetch(item.discordId);
+                    user.send(`😱 ${collection.data} floor price is 📉 to ${response.data.floorPrice / 1000000000} SOL !!`);
+                    collection.lastSent == 'floorLow';
+                }
+            } else if ((response.data.floorPrice / 1000000000) >= collection.floorHigh) {
+                if (collection.lastSent != 'floorHigh') {
+                    let user = await bot.users.fetch(item.discordId);
+                    user.send(`🚀 ${collection.data} floor price is 📈 to ${response.data.floorPrice / 1000000000} SOL !!`);
+                    collection.lastSent == 'floorHigh';
+                }
             }
         });
+        await dashboardKeys.updateOne({ discordId: item.discordId }, item);
     });
 }
