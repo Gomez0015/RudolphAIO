ae6f5743313323c42fcd18f33d176466be76010c
Gomez0015 <oscargomez.raxo@gmail.com>
2022-03-04 11:42:18 +0100

    Wallet Monitor

--- controllers/monitor.js
+++ controllers/monitor.js
@@ -1,5 +1,6 @@
 const dashboardKeys = require('../commands/models/dashboardKeysModel.js');
 const axios = require('axios');
+const { MessageEmbed } = require('discord.js');
 
 exports.checkMonitors = async function(bot) {
     const dashboardKeysData = await dashboardKeys.find({});
@@ -30,7 +31,23 @@ exports.checkMonitors = async function(bot) {
 
             if (wallet.lastSent != response[0]) {
                 let user = await bot.users.fetch(item.discordId);
-                await user.send(JSON.stringify(response[0]));
+                const walletEmbed = new MessageEmbed()
+                    .setTitle('New Wallet Activity')
+                    .setURL(`https://api-mainnet.magiceden.dev/v2/wallets/${wallet.data}/activities?offset=0&limit=1`)
+                    .addFields({
+                        name: 'Type',
+                        value: response[0].type
+                    }, {
+                        name: 'Collection',
+                        value: response[0].collection
+                    }, {
+                        name: 'Price',
+                        value: response[0].price,
+                    }, )
+                    .setTimestamp()
+                    .setFooter({ text: 'RudolphAIO Monitors', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+                await user.send({ embeds: [walletEmbed] });
                 wallet.lastSent = response[0];
                 await dashboardKeys.updateOne({ discordId: item.discordId }, item);
             }
