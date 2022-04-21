c4401a852ba3117bafa667f8457c2632fc66819f
Gomez0015 <oscargomez.raxo@gmail.com>
2022-04-21 15:26:43 +0200

    Added expired key checking

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,21 @@
 const dashboardKeys = require('../commands/models/dashboardKeysModel.js');
 
+function getNumberOfDays(start, end) {
+    const date1 = new Date(start);
+    const date2 = new Date(end);
+
+    // One day in milliseconds
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    // Calculating the time difference between two dates
+    const diffInTime = date2.getTime() - date1.getTime();
+
+    // Calculating the no. of days between two dates
+    const diffInDays = Math.round(diffInTime / oneDay);
+
+    return diffInDays;
+}
+
 exports.checkExpiry = async function(bot) {
     const dashboardKeysData = await dashboardKeys.find({});
 
@@ -13,6 +29,8 @@ exports.checkExpiry = async function(bot) {
                     member.roles.remove(member.roles.cache);
                 })
                 .catch(console.error);
+        } else if (getNumberOfDays(item.start_date, new Date()) >= 31) {
+            console.log('Expired: ' + item.discordId);
         }
     });
 }
