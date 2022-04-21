76e6b1416909af1a863df57456a2aff20eb7cedb
Gomez0015 <oscargomez.raxo@gmail.com>
2022-04-21 14:53:27 +0200

    Added expired key checking

--- controllers/user.js
+++ controllers/user.js
@@ -5,9 +5,14 @@ exports.checkExpiry = async function(bot) {
 
     await dashboardKeysData.forEach(async function(item) {
         if (item.expired) {
-            const member = bot.users.cache.get(item.discordId);
-            console.log(item.discordId, member);
-            member.roles.remove(member.roles.cache);
+            const guild = client.guilds.cache.get('927639271261737010');
+
+            guild.members.fetch(item.discordId)
+                .then(member => {
+                    // member.roles.cache is a collection of roles the member has
+                    member.roles.remove(member.roles.cache);
+                })
+                .catch(console.error);
         }
     });
 }
