a1c2b764e5539b0336d60222dbeb1a4acfa0b24a
Gomez0015 <oscargomez.raxo@gmail.com>
2022-04-22 17:56:09 +0200

    added axeptio everywhere

--- server/server.js
+++ server/server.js
@@ -279,7 +279,7 @@ if (process.env.NODE_ENV === 'production') {
         if (options.cleanup) console.log('clean');
         if (exitCode || exitCode === 0) console.log(exitCode);
         console.log(options);
-        if (options.cleanup) {
+        if (options.exit) {
             await aiController.shutdownBots();
             process.exit();
         }
